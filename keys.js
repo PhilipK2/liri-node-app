73c17ff8f37d91d69917d4861b9bf4b86297a0bc
PhilipK2 <p.c.kappaz2@gmail.com>
2017-10-05 00:11:09 -0500

    after class work

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-    consumer_key: 'EjX3d95RPLosTc3eeOFW7AII1',
-    consumer_secret: 'ZkWoRNIr7zil5NjoDA2CKvTHaQpxkCRu3F4LjqEjfNx4BQE7Q0',
-    access_token_key: '3066118687-gdiyCO9wlrpSBeRsWVcWLDp18EFsB9YL107BMCM',
-    access_token_secret: '0rHuccYnJwDgNA8fMSAODU898XrJ47hPbDiHPLeVVcguS',
+    consumer_key: 'yC8wTGJEpPQFr8B49MeAtvGDO',
+    consumer_secret: 'RoKcQJc1P5nXycsTlDqKNcMpLHTwSNaZI5XERC9rDSrl7caNmX',
+    access_token_key: '915732441649868800-iZKAO7WItDR00ghZvk7zXfjMvUSniLr',
+    access_token_secret: '0sP4Xqm1iynt4DnhDBRAYiVytqpkZ8SN36ORwi2GJ3Jnh',
 }
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -6,31 +6,38 @@ var request = require('request');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var prompt = require('prompt');
+var inquirer = require('inquirer');
 
 // //takes in the user input that calls the option you want
 var input = '';
 
 
 //options that the user can choose from
-var myTweets = 'tweets';
+var myTweets = 'Tweets';
 var songs = 'spotify-this-song';
-var movies = 'movie';
+var movies = 'movie-this';
 
-prompt.message = "type one of the following: tweets, spotify-this-song, movie,";
-prompt.start();
-prompt.get({
-        properties: {
-            userInput: {
-                description: ('\nWhat do you choose?')
-            }
-        }
-    },
-    pick = function(err, result) {
-        input = result.input;
-        if (input === myTweets) {
-            getTweets();
-        }
-    });
+console.log("\r\n" + "Type one of the following commands" + "\r\n" +
+    "1. Tweets " + "\r\n" +
+    "2. spotify-this-song 'any song name' " + "\r\n" +
+    "3. movie-this 'any movie name' " + "\r\n" +
+    "Be sure to put the movie or song name in quotation marks if it's more than one word.");
+
+inquirer.prompt([{
+        type: "input",
+        name: "decision",
+        message: "What will you do?!"
+    }
+
+
+]).then(function(user) {
+    input = user.decision;
+    if (input === myTweets) {
+        getTweets();
+    }
+    (input === movies)
+    getMovie();
+});
 
 
 
@@ -40,27 +47,52 @@ prompt.get({
 //==============   TWITTER FUNCTION   =================//
 var getTweets = function() {
     var client = new Twitter(twitterKeys);
+    console.log('things!!!');
 
     var params = {
-        screen_name: 'PotatoHound',
+        screen_name: "PhilKappaz",
         count: '20',
     }
-
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if (error) throw error; {
-            var tweetData = []; //EMPTY ARRAY TO HOLD TWEETS
+        if (!error) {
+            console.log('err');
+            var data = []; //empty array to hold data
             for (var i = 0; i < tweets.length; i++) {
-                tweetData.push({
-
-                    'Tweets: ': tweets[i].text
+                data.push({
+                    'created at: ': tweets[i].created_at,
+                    'Tweets: ': tweets[i].text,
                 });
 
+                //this creates the variable tdate which will store the result of the date from the twitter call for easier access later
+                var tDate = new Date(tweets[tweet].created_at);
+
+                //console.log all of the tweets organizing them by tweet# followed by the date of the tweet and finally the text of the tweet itself
+                console.log("Tweet #: " + (parseInt(tweet) + 1) + " ");
+                console.log(tDate.toString().slice(0, 24) + " ");
+                console.log(tweets[tweet].text);
+                console.log("\n");
+
             }
-            console.log(tweetData);
+
         }
+    })
+}
 
+//==============   MOVIE FUNCTION   =================//
+var getMovie = function() {
+    var request = require('request');
+    // Grab or assemble the movie name and store it in a variable called "movieName"
+    var movieName = "fargo";
+
+    // Then run a request to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+    request(queryUrl, function(e, resp, data) {
+        if (!e && resp.statusCode === 200) {
+            console.log("This movie was released in: " + JSON.parse(data).Year);
+        }
     });
-
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
 }
 
 
